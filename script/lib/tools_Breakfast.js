465bd45ab8381e766bd26ace5175c5e97d511b92
Alexis Pennel <alexispennel@gmail.com>
2023-02-03 15:04:30 +0100

    breakfast monday 80%

---
+++ script/lib/tools_Breakfast.js
@@ -0,0 +1,32 @@
+const removeSelectedRecipeB = (day) => {
+    // if (day === "tuesday") {
+    //     document.getElementById("0tuesday").classList.remove("popUpCardSelect");
+    //     document.getElementById("1tuesday").classList.remove("popUpCardSelect");
+    //     document.getElementById("2tuesday").classList.remove("popUpCardSelect");
+    //     document.getElementById("3tuesday").classList.remove("popUpCardSelect");
+    //     document.getElementById("4tuesday").classList.remove("popUpCardSelect");
+    //     document.getElementById("5tuesday").classList.remove("popUpCardSelect");
+    //     document.getElementById("6tuesday").classList.remove("popUpCardSelect");
+    //     document.getElementById("7tuesday").classList.remove("popUpCardSelect");
+    //     document.getElementById("8tuesday").classList.remove("popUpCardSelect");
+    //     document.getElementById("9tuesday").classList.remove("popUpCardSelect");
+    //     document.getElementById("10tuesday").classList.remove("popUpCardSelect");
+    //     document.getElementById("11tuesday").classList.remove("popUpCardSelect");
+    //     return
+    // };
+
+    document.getElementById("B0").classList.remove("popUpCardSelect");
+    document.getElementById("B1").classList.remove("popUpCardSelect");
+    document.getElementById("B2").classList.remove("popUpCardSelect");
+    document.getElementById("B3").classList.remove("popUpCardSelect");
+    document.getElementById("B4").classList.remove("popUpCardSelect");
+    document.getElementById("B5").classList.remove("popUpCardSelect");
+    document.getElementById("B6").classList.remove("popUpCardSelect");
+    document.getElementById("B7").classList.remove("popUpCardSelect");
+    document.getElementById("B8").classList.remove("popUpCardSelect");
+    document.getElementById("B9").classList.remove("popUpCardSelect");
+    document.getElementById("B10").classList.remove("popUpCardSelect");
+    document.getElementById("B11").classList.remove("popUpCardSelect");
+};
+
+export { removeSelectedRecipeB }
--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
-import { addSelectedButton, removeSelectedButton, removeSelectedRecipe, removeVisibilityHidden} from "./lib/tools.js";
+import { addSelectedButton, removeSelectedButton, removeSelectedRecipe, removeVisibilityHidden } from "./lib/tools_Lunch.js";
+import { removeSelectedRecipeB } from "./lib/tools_Breakfast.js";
 
 const main = () => {
     // Variables 
@@ -7,13 +8,16 @@ const main = () => {
     // Variables popUp MONDAY 
     const cardContainerMonday = document.getElementById('cardContainerMonday')
     const popUpMonday = document.getElementById('popUpMonday');
+    const popUpMondayB = document.getElementById('popUpMondayB');
+    let cardMondayB = "";
     let cardMonday = "";
 
     // Variable Tableau de recettes
     const recipesArray = [];
-
+    const breakfastRecipesArray = [];
     // Variable element select
     let elementSelected = -1;
+    let elementSelectedB = -1;
 
 
     // Bouton "new plan"
@@ -22,49 +26,113 @@ const main = () => {
         console.log(localStorage)
         removeSelectedButton();
     });
-    
+
+    // Apparition popUp "Breakfast" 
+    document.getElementById("mondayBreakfast").addEventListener("click", () => {
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpMondayB.classList.remove('visibilityHidden');
+        }, 400)
+    });
+
+    document.getElementById("tuesdayBreakfast").addEventListener("click", () => {
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpTuesday.classList.remove('visibilityHidden');
+        }, 400)
+    });
+
+    document.getElementById("wednesdayBreakfast").addEventListener("click", () => {
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpWednesday.classList.remove('visibilityHidden');
+        }, 400)
+    });
+
+    document.getElementById("thursdayBreakfast").addEventListener("click", () => {
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpThursday.classList.remove('visibilityHidden');
+        }, 400)
+    });
+
+    document.getElementById("fridayBreakfast").addEventListener("click", () => {
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpFriday.classList.remove('visibilityHidden');
+        }, 400)
+    });
+
+    document.getElementById("saturdayBreakfast").addEventListener("click", () => {
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpSaturday.classList.remove('visibilityHidden');
+        }, 400)
+    });
+
+    document.getElementById("sundayBreakfast").addEventListener("click", () => {
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpSunday.classList.remove('visibilityHidden');
+        }, 400)
+    });
 
     // Apparition popUp "Lunch" par jour de la semaine  
     document.getElementById("mondayLunch").addEventListener("click", () => {
-        main.classList.add('visibilityHidden');
-        popUpMonday.classList.remove('visibilityHidden');
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpMonday.classList.remove('visibilityHidden');
+        }, 400)
     });
 
     document.getElementById("tuesdayLunch").addEventListener("click", () => {
-        main.classList.add('visibilityHidden');
-        popUpTuesday.classList.remove('visibilityHidden');
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpTuesday.classList.remove('visibilityHidden');
+        }, 400)
     });
 
     document.getElementById("wednesdayLunch").addEventListener("click", () => {
-        main.classList.add('visibilityHidden');
-        popUpWednesday.classList.remove('visibilityHidden');
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpWednesday.classList.remove('visibilityHidden');
+        }, 400)
     });
 
     document.getElementById("thursdayLunch").addEventListener("click", () => {
-        main.classList.add('visibilityHidden');
-        popUpThursday.classList.remove('visibilityHidden');
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpThursday.classList.remove('visibilityHidden');
+        }, 400)
     });
 
     document.getElementById("fridayLunch").addEventListener("click", () => {
-        main.classList.add('visibilityHidden');
-        popUpFriday.classList.remove('visibilityHidden');
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpFriday.classList.remove('visibilityHidden');
+        }, 400)
     });
 
     document.getElementById("saturdayLunch").addEventListener("click", () => {
-        main.classList.add('visibilityHidden');
-        popUpSaturday.classList.remove('visibilityHidden');
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpSaturday.classList.remove('visibilityHidden');
+        }, 400)
     });
 
     document.getElementById("sundayLunch").addEventListener("click", () => {
-        main.classList.add('visibilityHidden');
-        popUpSunday.classList.remove('visibilityHidden');
+        setTimeout(() => {
+            main.classList.add('visibilityHidden');
+            popUpSunday.classList.remove('visibilityHidden');
+        }, 400)
     });
 
     //*************************Bouton return 
     document.addEventListener("click", (e) => {
         if (e.target.className === "fa-solid fa-arrow-left") {
-            popUpTuesday.classList.add("visibilityHidden");
+            popUpMondayB.classList.add('visibilityHidden');
             popUpMonday.classList.add("visibilityHidden");
+            popUpTuesday.classList.add("visibilityHidden");
             popUpWednesday.classList.add("visibilityHidden");
             popUpThursday.classList.add("visibilityHidden");
             // popUpFriday.classList.add("visibilityHidden");
@@ -84,14 +152,23 @@ const main = () => {
             this.ingredients = ingredients;
         }
     };
-    // ********************** Push recettes tableau 
+
+    // Recette "breakfast"
+    breakfastRecipesArray.push(new recipe("B0", "Huevo", ["Huevos", "Cebolla", "Tomates", "Pan"]));
+    breakfastRecipesArray.push(new recipe("B1", "Sandwich de atun", ["Atun", "Pan", "Aguacate", "Lechuga", "Cebolla"]));
+    breakfastRecipesArray.push(new recipe("B2", "Sandwich de jamon y queso", ["Jamon", "Queso oaxaca", "Lechuga", "Pan", "Cebolla"]));
+    breakfastRecipesArray.push(new recipe("B3", "Sandwich de pollo", ["Pechuga de pollo", "Cebolla", "Queso oaxaca", "Pan"]));
+    breakfastRecipesArray.push(new recipe("B4", "Burritas", ["Tortillas", "Jamon", "Queso oaxaca", "Media crema", "Salsa verde"]));
+    breakfastRecipesArray.push(new recipe("B5", "Avena con leche y fruta", ["Avena", "Leche", "Fruta", "Canela"]));
+
+    // Recettes "Lunch and Dinner" 
     recipesArray.push(new recipe("0", "Pechuga a la plancha con verduras y pastas", ["Pechuga de pollo", "Cebolla", "Pastas", "Verduras", "Salsa tomate"]))
     recipesArray.push(new recipe("1", "Arroz con huevo", ["Arroz", "Huevos", "Tomates"]));
     recipesArray.push(new recipe("2", "Filete de pescado a la plancha con verduras", ["Filete de pescado", "Pastas o arroz", "Verduras", "Lechuga"]))
-    recipesArray.push(new recipe("3", "Salmon con verduras", ["Salmon", "Verduras", "Pastas o arroz", ]));
+    recipesArray.push(new recipe("3", "Salmon con verduras", ["Salmon", "Verduras", "Pastas o arroz",]));
     recipesArray.push(new recipe("4", "Tacos de pollo", ["Pechuga de pollo", "Tortillas", "Cebolla", "Media crema", "Aguacate"]));
     recipesArray.push(new recipe("5", "Tostadas de pollo", ["Pechuga de pollo", "Tostadas", "Lechuga", "Cebolla", "Aguacate", "Media crema", "Frijol"]));
-    recipesArray.push(new recipe("6", "Molletes fit", ["Pan", "Frijol", "Queso oaxaca", "Cebolla" ]));
+    recipesArray.push(new recipe("6", "Molletes fit", ["Pan", "Frijol", "Queso oaxaca", "Cebolla"]));
     recipesArray.push(new recipe("7", "Atun con salmas", ["Atun", "Lechuga", "Verduras", "Salmas"]));
     recipesArray.push(new recipe("8", "Huevo", ["Huevos", "Espinacas", "Tomates", "Pan"]));
     recipesArray.push(new recipe("9", "Omelette", ["Huevos", "Champinones", "Queso oaxaca", "Pan"]));
@@ -99,7 +176,106 @@ const main = () => {
     recipesArray.push(new recipe("11", "Hamburguesa casera", ["Pan hamburguesa", "Pechuga de pollo", "Cebolla", "Lechuga", "Queso oaxaca", "Mayonessa / Ketchup"]));
 
 
-    // Apparition cartes plats PopUp MONDAY
+// Apparition cartes plats PopUp MONDAY BREAKFAST
+
+    //********************** Fonction creation cartes PopUp MONDAY BREAKFAST
+    const createCardLunchMondayB = (element) => {
+        cardMondayB += `<a href="#" class="popUpCard flexCenter" id="${element.number}">
+            <h2>${element.name}</h2>
+    </a>`
+        cardContainerMondayB.innerHTML = cardMondayB;
+    }
+    // ********************* Boucle pour crea cartes pour chaques recettes (BREAKFAST)
+    breakfastRecipesArray.forEach(element => {
+        createCardLunchMondayB(element);
+    });
+
+    document.getElementById('B0').addEventListener("click", (e) => {
+        elementSelectedB = 0;
+        document.getElementById("B0").classList.add("popUpCardSelect");
+        document.getElementById("B1").classList.add("visibilityHidden");
+        document.getElementById("2B").classList.add("visibilityHidden");
+        document.getElementById("B3").classList.add("visibilityHidden");
+        document.getElementById("B4").classList.add("visibilityHidden");
+        document.getElementById("B5").classList.add("visibilityHidden");
+        document.getElementById("B6").classList.add("visibilityHidden");
+    });
+
+    document.getElementById('B1').addEventListener("click", (e) => {
+        elementSelectedB = 1;
+        document.getElementById("B1").classList.add("popUpCardSelect");
+        document.getElementById("B0").classList.add("visibilityHidden");
+        document.getElementById("B2").classList.add("visibilityHidden");
+        document.getElementById("B3").classList.add("visibilityHidden");
+        document.getElementById("B4").classList.add("visibilityHidden");
+        document.getElementById("B5").classList.add("visibilityHidden");
+        document.getElementById("B6").classList.add("visibilityHidden");
+    });
+
+    document.getElementById('B2').addEventListener("click", (e) => {
+        elementSelectedB = 2;
+        document.getElementById("B2").classList.add("popUpCardSelect");
+        document.getElementById("B1").classList.add("visibilityHidden");
+        document.getElementById("B0").classList.add("visibilityHidden");
+        document.getElementById("B3").classList.add("visibilityHidden");
+        document.getElementById("B4").classList.add("visibilityHidden");
+        document.getElementById("B5").classList.add("visibilityHidden");
+        document.getElementById("B6").classList.add("visibilityHidden");
+    });
+
+    document.getElementById('B3').addEventListener("click", (e) => {
+        elementSelectedB = 3;
+        document.getElementById("B3").classList.add("popUpCardSelect");
+        document.getElementById("B1").classList.add("visibilityHidden");
+        document.getElementById("B2").classList.add("visibilityHidden");
+        document.getElementById("B0").classList.add("visibilityHidden");
+        document.getElementById("B4").classList.add("visibilityHidden");
+        document.getElementById("B5").classList.add("visibilityHidden");
+        document.getElementById("B6").classList.add("visibilityHidden");
+    });
+
+    document.getElementById('B4').addEventListener("click", (e) => {
+        elementSelectedB = 4;
+        document.getElementById("B4").classList.add("popUpCardSelect");
+        document.getElementById("B1").classList.add("visibilityHidden");
+        document.getElementById("B2").classList.add("visibilityHidden");
+        document.getElementById("B3").classList.add("visibilityHidden");
+        document.getElementById("B0").classList.add("visibilityHidden");
+        document.getElementById("B5").classList.add("visibilityHidden");
+        document.getElementById("B6").classList.add("visibilityHidden");
+    });
+
+    document.getElementById('B5').addEventListener("click", (e) => {
+        elementSelectedB = 5;
+        document.getElementById("B5").classList.add("popUpCardSelect");
+        document.getElementById("B1").classList.add("visibilityHidden");
+        document.getElementById("B2").classList.add("visibilityHidden");
+        document.getElementById("B3").classList.add("visibilityHidden");
+        document.getElementById("B4").classList.add("visibilityHidden");
+        document.getElementById("B0").classList.add("visibilityHidden");
+        document.getElementById("B6").classList.add("visibilityHidden");
+    });
+
+    //************************ Bouton reset BREAKFAST MONDAY 
+    document.getElementById("resetButtonMondayB").addEventListener("click", () => {
+        elementSelectedB = -1;
+        removeSelectedRecipeB();
+        removeVisibilityHidden();
+        removeSelectedButton("monday");
+        localStorage.removeItem("mondayLunch");
+    });
+
+
+
+
+
+
+
+
+
+
+
+    // Apparition cartes plats PopUp MONDAY LUNCH
 
     //********************** Fonction creation cartes PopUp MONDAY
     const createCardLunchMonday = (element) => {
@@ -343,7 +519,7 @@ const main = () => {
 
     // Bouton validation du plan 
     // document.getElementById("validateButton").addEventListener("click", () => {
-      
+
     // });
 };
 
